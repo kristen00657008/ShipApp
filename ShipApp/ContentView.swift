ce09d3bf244d7c9aaa601adebecfade3b9f3b3a3
“Chris <hh0975832701@gmail.com>
2020-03-24 12:02:09 +0800

    Initial Commit

---
+++ ShipApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShipApp
+//
+//  Created by User24 on 2020/3/24.
+//  Copyright © 2020 User24. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
